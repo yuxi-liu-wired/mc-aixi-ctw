da242309b9f8f750ec642cbb44d82efe0de267b0
Wenxi Wu <wwx@Wenxis-MacBook-Pro.local>
2019-09-30 12:57:49 +1000

    bug fixed, rewrite reward calculation

--- pyaixi/environments/extended_tiger.py
+++ pyaixi/environments/extended_tiger.py
@@ -22,11 +22,11 @@ description = 'extended_tiger.txt'
 tiger_action_enum = util.enum('stand','listen','open_left_door','open_right_door')
 print(tiger_action_enum)
 tiger_observation_enum = util.enum('left','right','void')
-tiger_reward_enum = util.enum(penalty = 90, eaten = 0, gold = 130, normal = 99)
 tiger_state_enum = util.enum('sitting', 'standing')
 
 sitting = tiger_state_enum.sitting
 standing = tiger_state_enum.standing
+
 stand = tiger_action_enum.stand
 listen = tiger_action_enum.listen
 open_left = tiger_action_enum.open_left_door
@@ -35,12 +35,6 @@ open_right = tiger_action_enum.open_right_door
 left = tiger_observation_enum.left
 right = tiger_observation_enum.right
 void = tiger_observation_enum.void
-
-penalty = tiger_reward_enum.penalty
-normal = tiger_reward_enum.normal
-eaten = tiger_reward_enum.eaten
-gold = tiger_reward_enum.gold
-
 
 class ExtendedTiger(environment.Environment):
     """ A biased coin is flipped and the agent is tasked with predicting how it
@@ -63,10 +57,6 @@ class ExtendedTiger(environment.Environment):
                          Default value is `default_probability`.
                          (Optional.)
     """
-
-    # Instance attributes.
-
-    # Set the default probability for the biased coin, when none is supplied from the options.
     default_probability = 0.85
 
     # Instance methods.
@@ -90,14 +80,15 @@ class ExtendedTiger(environment.Environment):
         # Defines the acceptable observation values.
         self.valid_observations = list(tiger_observation_enum.keys())
         
-        self.valid_rewards = list(tiger_reward_enum.keys())
+        self.valid_rewards = range(2**8)
         # Set an initial percept.
-        self.reward = 0
+        self.reward = 128
         self.is_finished = False
         self.state = sitting
         self.observation = void
         self.tiger = left if random.randint(0,1) == 1 else right
     # end def
+
 
     def perform_action(self, action):
         """ Receives the agent's action and calculates the new environment percept.
@@ -108,11 +99,11 @@ class ExtendedTiger(environment.Environment):
         self.action = action
         if self.state == sitting:
             if action == stand:
-                self.reward = normal
+                self.reward -= 1
                 self.state = standing
             elif action == listen:
-                self.reward = normal
-                if (random.random() < self.default_probability):
+                self.reward -= 1
+                if (random.random() <= self.default_probability):
                     self.observation = self.tiger
                 else:
                     if self.tiger == left:
@@ -120,29 +111,29 @@ class ExtendedTiger(environment.Environment):
                     else:
                         self.observation = left
             else:
-                self.reward = penalty
+                self.reward -= 10
         else:
             if action == open_left:
                 if self.tiger == left:
-                    self.reward = eaten
+                    self.reward -= 100
                 else:
-                    self.reward = gold
-                self.print()
+                    self.reward += 30
                 self.clear_start()
+                self.is_finished = True
             elif action == open_right:
                 if self.tiger == right:
-                    self.reward = eaten
+                    self.reward -= 100
                 else:
-                    self.reward = gold
-                self.print()
+                    self.reward += 30
                 self.clear_start()
+                self.is_finished = True
             else:
-                self.reward = penalty
+                self.reward -= 10
         return (self.observation, self.reward)
     # end def
     def clear_start(self):
         print("Game Over! Starting New Game...")
-        self.reward = 0
+        self.reward = 128
         self.state = sitting
         self.tiger = left if random.randint(0,1) == 1 else right
         print(f"Tiger be set at the :{self.tiger}")
