51067d8562fc742fd0e91fce438baf855c5dee60
AndrewTanggara <u6797423@anu.edu.au>
2019-10-01 13:23:41 +1000

    fixed revert to every set of sampling (put revert in sample_iteration())

--- pyaixi/search/monte_carlo_search_tree.py
+++ pyaixi/search/monte_carlo_search_tree.py
@@ -108,40 +108,37 @@ class MonteCarloSearchNode:
         """
 
         reward_sum = 0.0
-        agent.set_savestate()
 
         if horizon == 0:
-            print("(-)reached horizon")
+#            print("(-)reached horizon")
             assert self.type == decision_node
             return 0.0
 
         elif self.type == chance_node: #Sample Percept if this is a chance_node
-            print("(.)sample percept")
+#            print("(.)sample percept")
             # sample or from ρ(or|h)
             observation, reward = agent.generate_percept_and_update()
-            print("\t-- receive obs, rwd: "+str(observation)+", "+str(reward))
+#            print("\t-- receive obs, rwd: "+str(observation)+", "+str(reward))
             # create node hor if it is an unvisited child.
             if not (observation in self.children):
                 self.children[observation] = MonteCarloSearchNode(decision_node)
             # end if
             child = self.children[observation]
-            print("\t-- current horizon: "+str(horizon-1))
+#            print("\t-- current horizon: "+str(horizon-1))
             reward_sum = reward + child.sample(agent, horizon - 1)
 
         elif self.visits == 0: #Rollout if decision_node hasnt been visited
-            print("(/\)havent visited decision node before")
+#            print("(/\)havent visited decision node before")
             reward_sum = agent.playout(horizon)
-            print("\t-- rollout, got expected reward: "+str(reward_sum))
+#            print("\t-- rollout, got expected reward: "+str(reward_sum))
 
         else: #Select Action if this is a decision_node
-            print("(*)select action")
+#            print("(*)select action")
             action = self.select_action(agent, horizon)
             agent.model_update_action(action)
             reward_sum = self.children[action].sample(agent, horizon)
-            print("\t-- get reward sum: "+str(reward_sum))
+#            print("\t-- get reward sum: "+str(reward_sum))
         # end if
-
-        agent.restore_savestate()
 
         # update node
         self.mean = (reward_sum + self.mean * self.visits)/(self.visits + 1)
@@ -158,9 +155,10 @@ class MonteCarloSearchNode:
             - `horizon`: how many steps into the future to sample
             - `iterations`: how many iterations to perform
         """
-
+        agent.set_savestate()
         for i in range(iterations):
             self.sample(agent, horizon)
+        agent.restore_savestate()
     # end def
 
     def select_action(self, agent, horizon):
@@ -178,7 +176,7 @@ class MonteCarloSearchNode:
 
         if untried_actions:  # If there are untried actions
             action = random.choice(untried_actions) # choose a random untried action
-            print("\t-- try untried action: "+str(action))
+#            print("\t-- try untried action: "+str(action))
             self.children[action] = MonteCarloSearchNode(chance_node) # add it as a new MCTS node
             return action # return this action
         
@@ -195,7 +193,7 @@ class MonteCarloSearchNode:
     
             # now pick the action with the highest UCB score.
             best_action = max(action_ucb, key=action_ucb.get)
-            print("\t -- action picked through UCB: "+str(action))
+#            print("\t -- action picked through UCB: "+str(action))
             return best_action
         # end if
     # end def
@@ -211,7 +209,6 @@ def mcts_planning(agent, horizon, iterations):
         - `horizon`: how many cycles into the future to sample
         - `iterations`: how many samples to take
     """
-    print("START MCTS\n(.) is chance node ; (*) is decision node ; (/\) is a rollout \n=========================")
     mc_tree = MonteCarloSearchNode(decision_node)
     mc_tree.sample_iterations(agent, horizon, iterations)
     return mc_tree.select_action(agent, horizon)
