c5743945403276294d819d226e7e7214f825a2dc
Jiayan Liu <u6107041@anu.edu.au>
2019-10-03 14:20:25 +1000

    bug fixed on extended_tiger

--- pyaixi/environments/extended_tiger.py
+++ pyaixi/environments/extended_tiger.py
@@ -15,9 +15,6 @@ import random
 import sys
 import numpy as np
 
-PROJECT_ROOT = os.path.realpath(os.path.join(os.pardir, os.pardir))
-sys.path.insert(0, PROJECT_ROOT)
-
 from pyaixi import environment, util
 
 description = 'extended_tiger.txt'
@@ -25,11 +22,11 @@ description = 'extended_tiger.txt'
 tiger_action_enum = util.enum('stand','listen','open_left_door','open_right_door')
 print(tiger_action_enum)
 tiger_observation_enum = util.enum('left','right','void')
+tiger_reward_enum = util.enum(penalty = 90, eaten = 0, gold = 130, normal = 99)
 tiger_state_enum = util.enum('sitting', 'standing')
 
 sitting = tiger_state_enum.sitting
 standing = tiger_state_enum.standing
-
 stand = tiger_action_enum.stand
 listen = tiger_action_enum.listen
 open_left = tiger_action_enum.open_left_door
@@ -39,24 +36,37 @@ left = tiger_observation_enum.left
 right = tiger_observation_enum.right
 void = tiger_observation_enum.void
 
+penalty = tiger_reward_enum.penalty
+normal = tiger_reward_enum.normal
+eaten = tiger_reward_enum.eaten
+gold = tiger_reward_enum.gold
+
+
 class ExtendedTiger(environment.Environment):
-    """ 
-        extended tiger position prediction, a tiger behind in a door, player 
-        is asking to open one of the door(left or right), before open either 
-        door, player is sitting on a chair, and can listen the noise made by 
-        tiger, but it has a probility of 0.85 that player get correct observation.
+    """ A biased coin is flipped and the agent is tasked with predicting how it
+        will land. The agent receives a reward of `rWin` for a correct
+        prediction and `rLoss` for an incorrect prediction. The observation
+        specifies which side the coin landed on (`oTails` or `oHeads`).
+        The action corresponds to the agent's prediction for the
+        next coin flip (`aTails` or `aHeads`).
 
         Domain characteristics:
 
-        - environment: "extended_tiger"
-        - maximum action: 4 (2 bit)
-        - maximum observation: 3 (2 bit)
-        - maximum reward: 256 (8 bit)
+        - environment: "coin_flip"
+        - maximum action: 1 (1 bit)
+        - maximum observation: 1 (1 bit)
+        - maximum reward: 1 (1 bit)
+
+        Configuration options:
+        - `coin-flip-p`: the probability the coin lands on heads
+                         (`oHeads`). Must be a number between 0 and 1 inclusive.
+                         Default value is `default_probability`.
+                         (Optional.)
     """
 
-    """
-    default ptobability of correct observation given by action listenning
-    """
+    # Instance attributes.
+
+    # Set the default probability for the biased coin, when none is supplied from the options.
     default_probability = 0.85
 
     # Instance methods.
@@ -67,6 +77,8 @@ class ExtendedTiger(environment.Environment):
              - `options` is a dictionary of named options and their values.
 
             The following options in `options` are optional:
+            
+             - `coin-flip-p`: the probability that the coin will land on heads. (Defaults to 0.7.)
         """
 
         # Set up the base environment.
@@ -78,79 +90,73 @@ class ExtendedTiger(environment.Environment):
         # Defines the acceptable observation values.
         self.valid_observations = list(tiger_observation_enum.keys())
         
-        self.valid_rewards = range(2**8)
+        self.valid_rewards = list(tiger_reward_enum.keys())
         # Set an initial percept.
-        self.reward = 128 #given initial reward with 128
+        self.reward = 0
+        self.tmp_reward = 0
         self.is_finished = False
         self.state = sitting
         self.observation = void
         self.tiger = left if random.randint(0,1) == 1 else right
     # end def
 
-
     def perform_action(self, action):
         """ Receives the agent's action and calculates the new environment percept.
         """
+
         assert self.is_valid_action(action)
         # Save the action.
         self.action = action
-        print("perform action", action)
-        tmp_reward = 0
-        tmp_observation = void
-        if self.state == sitting:  
+        if self.state == sitting:
             if action == stand:
-                self.reward -= 1  #perform stand, when sitting given reward -1
+                self.reward = normal
                 self.state = standing
-                tmp_reward = self.reward
-            elif action == listen: #perform lsiten, when sitting given reward -1
-                self.reward -= 1
-                tmp_reward = self.reward
-                if (random.random() <= self.default_probability):
+            elif action == listen:
+                self.reward = normal
+                if (random.random() < self.default_probability):
                     self.observation = self.tiger
                 else:
                     if self.tiger == left:
                         self.observation = right
-                        tmp_observation = void
                     else:
                         self.observation = left
-                        tmp_observation = void
-            else: #other invalid action given reward -10
-                self.reward -= 10
-                tmp_reward = self.reward
+            else:
+                self.reward = penalty
         else:
-            if action == open_left:  
+            if action == open_left:
                 if self.tiger == left:
-                    self.reward -= 100 #open door with tiger when standing given reward -100
+                    self.reward = eaten
                 else:
-                    self.reward += 30  #open door with gold when standing given reward +30
-                tmp_reward = self.reward
-                tmp_observation = self.observation
-                self.clear_start()     #reset game state(position of tiger and state of player)
+                    self.reward = gold
+#                print("bug 2")
+#                self.state = sitting
+                self.clear_start()
             elif action == open_right:
                 if self.tiger == right:
-                    self.reward -= 100
+                    self.reward = eaten
                 else:
-                    self.reward += 30
-                tmp_reward = self.reward
-                tmp_observation = self.observation
+                    self.reward = gold
+#                print("BUG 1")
+#                self.state = sitting
                 self.clear_start()
             else:
-                self.reward -= 10
-        if tmp_reward < 0:
-            tmp_reward = 0
-        return tmp_observation, tmp_reward
+                self.reward = penalty
+        self.tmp_reward += (self.reward - 100)
+#        print("PASSING")
+        return (self.observation, self.reward)
     # end def
     def clear_start(self):
-        print("Game Over! Starting New Game...")
-        self.reward = 128
-        self.observation = void
+#        print("Game Over! Starting New Game...")
+        self.tmp_reward = 0
         self.state = sitting
         self.tiger = left if random.randint(0,1) == 1 else right
-        print(f"Tiger be set at the :{self.tiger}")
+#        print(f"Tiger be set at the :{self.tiger}")
         
     def print(self):
         print("==" * 20)
-        print(f"Reward :{self.reward-100}")
+        print(f"Total Reward:{self.tmp_reward}")# Not able to show the last reward
+        
+        print(f"Action Reward :{self.reward-100}")
         print(f"State :{tiger_state_enum[self.state]}")
         print(f"Actions :{tiger_action_enum[self.action]}")
         print(f"Tiger is at {tiger_observation_enum[self.tiger]}")
