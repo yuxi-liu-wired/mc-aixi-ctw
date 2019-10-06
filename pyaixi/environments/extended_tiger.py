e69c94d767e6ec8acaf02453cb517e328e5952d9
Yuxi Liu <33951560+yuxiliu1995@users.noreply.github.com>
2019-10-07 01:41:24 +1100

    Fixed grammar issues and such in `extended_tiger.py`

--- pyaixi/environments/extended_tiger.py
+++ pyaixi/environments/extended_tiger.py
@@ -22,24 +22,31 @@ sys.path.insert(0, PROJECT_ROOT)
 
 from pyaixi import environment, util
 
-# extended tiger has 4 actions in total encoded in 2 bits
+
+# 4 actions, encoded in 2 bits.
 tiger_action_enum = util.enum('stand','listen','open_left_door','open_right_door')
-# Oberservation encoded into 2 bits
-tiger_observation_enum = util.enum('left','right','void') # Observation enum may need to be modified to satisfy 3 bits
-# Reward is encoded into 8 bits range from 0 to 130
+
+# 3 Oberservations, encoded in 2 bits.
+# 'left': the tiger is behind the left door.
+# 'right': the tiger is behind the right door.
+# 'void': the agent tried to listen while standing, and thus heard nothing.
+tiger_observation_enum = util.enum('left','right','void')
+
+# Reward ranges from 0 to 130, encoded in 8 bits.
 # 0(-100) reward is given by choosing the worst action: open the door with tiger hiding behind
 # 90(-10) reward is given by choosing an 'invalid' action: e.g. stand while standing
 # 99(-1)  reward is given by choosing an 'valid' action: e.g. sitting -> stand
 # 130(30) reward is given by choosing the best action: open the door with gold behind
-# 0, and 90 reward are the cases agent wants to avoid
+# The agent should aim to achieve the 130 reward.
 tiger_reward_enum = util.enum(penalty = 90, eaten = 0, gold = 130, normal = 99)
-# envrionment state: whethere the player is sitting or standing
+
+# 2 states, encoded in 1 bit.
 tiger_state_enum = util.enum('sitting', 'standing')
 
-# for coding convenience
-
+# Defines some shorthand notation for ease of reference.
 sitting = tiger_state_enum.sitting
 standing = tiger_state_enum.standing
+
 stand = tiger_action_enum.stand
 listen = tiger_action_enum.listen
 open_left = tiger_action_enum.open_left_door
@@ -56,30 +63,29 @@ gold = tiger_reward_enum.gold
 
 
 class ExtendedTiger(environment.Environment):
-    """ A tiger and a pot of gold are hidden behind one of two doors. Initially 
+    """ A tiger and a pot of gold are hidden behind one of two doors. Initially
         the agent begins sitting down on a chair. The agent has a choice of one
-        of four actions: stand, listen, open left door, open right door. While
-        sitting, the actions are valid to environment are "listen" and "stand".
-        Each action would resulting in a reward of -1 while any other "invalid"
-        actions would result in a penalty reward of -10. While standing, the
-        actions valid to environment are "open left door" and "open right door"
-        and rewards are same as state = sitting. Our goal is to let agent open
-        the door with gold behind in best sequence of actions(highest reward).
-        The game will end and restart by open any door.
-        
+        of four actions: stand, listen, open left door, open right door.
+
+        While sitting, the valid actions are "listen" and "stand". Each valid
+        action results in a reward of -1, invalid actions -10.
+
+        While standing, the valid actions are "open left door" and "open right
+        door". Opening the door to the gold results in reward 30. Opening the
+        door to the tiger results in reward -100.
+
+        The game will end and restart by opening any door.
+
         Domain characteristics:
-            
-        - environment: "exxtended_tiger"
-        - maximum action: 3 (2 bit)
-        - maximum observation: 3 (2 bit)
-        - maximum reward: 130 (8 bit)
-        
-        Configuration options:
-        - ''
+
+        - environment: "extended_tiger"
+        - maximum action: 3 (2 bits)
+        - maximum observation: 3 (2 bits)
+        - maximum reward: 130 (8 bits)
     """
     # Instance attributes.
 
-    # Set the default probability for listening(agent has 0.85 chance to predict the correct location by performing listen)
+    # By default, agent has 0.85 chance to hear the tiger.
     default_probability = 0.85
 
     # Instance methods.
@@ -87,11 +93,11 @@ class ExtendedTiger(environment.Environment):
     def __init__(self, options = {}):
         """ Construct the ExtendedTiger environment from the given options.
 
-             - `options` is a dictionary of named options and their values.
+             - `options`: a dictionary of named options and their values.
 
             The following options in `options` are optional:
-            
-             - `extended-tiger-p`: the probability that the agent will find the tiger. (Defaults to 0.85)
+
+             - `extended-tiger-p`: the probability that the agent will hear the tiger. (Defaults to 0.85)
         """
 
         # Set up the base environment.
@@ -102,13 +108,16 @@ class ExtendedTiger(environment.Environment):
 
         # Defines the acceptable observation values.
         self.valid_observations = list(tiger_observation_enum.keys())
-        
+
+        # Defines the acceptable reward values.
         self.valid_rewards = list(tiger_reward_enum.keys())
-        # Set an initial percept.
+
+        # Randomly initialize tiger location.
+        self.tiger = left if random.randint(0,1) == 1 else right
+
         self.reward = 0
         self.tmp_reward = 0 # restore temprory reward to display sum of reward of a single game run
-        self.state = sitting # represent the state of agent(sitting/standing) initially set to be sitting
-        self.tiger = left if random.randint(0,1) == 1 else right # set initial position of tiger
+        self.state = sitting # Agent starts sitting.
         self.observation = void
     # end def
 
@@ -117,59 +126,65 @@ class ExtendedTiger(environment.Environment):
         """
 
         assert self.is_valid_action(action)
-        # Save the action.
         self.action = action
-        # Conditions while sitting
+
         if self.state == sitting:
-            if action == stand:
-                self.reward = normal# reward by performing valid actions
-                self.state = standing# change state to standing 
+            if action == stand: # Valid action reward.
+                self.reward = normal
+                self.state = standing
             elif action == listen:
-                self.reward = normal# reward by performing valid actions
-                if (random.random() < self.default_probability): # give chance to correct predicting tiger location
+                self.reward = normal # Valid action reward.
+                # Randomly decide if the agent heard the tiger correctly.
+                if (random.random() < self.default_probability):
                     self.observation = self.tiger
                 else:
-                    # while the chance does not happen, give a misleading info
                     if self.tiger == left:
                         self.observation = right
                     else:
                         self.observation = left
-            else:
-                # reward by performing invalid action
+            else: # Invalid action reward.
                 self.reward = penalty
-        # Conditions while standing
+            # end if
         else:
-            if action == open_left: # Open left door
+            assert self.state == standing
+            if action == open_left:
                 if self.tiger == left:
-                    self.reward = eaten # if tiger is behind left door you got eaten!
-                else:
-                    self.reward = gold # otherwise, You find the GOLD!
-                self.clear_start() # initiate the game 
-            elif action == open_right: 
-                if self.tiger == right: # Open right door
                     self.reward = eaten
                 else:
                     self.reward = gold
-#                print("BUG 1")
-#                self.state = sitting
                 self.clear_start()
-            else:
+            elif action == open_right:
+                if self.tiger == right:
+                    self.reward = eaten
+                else:
+                    self.reward = gold
+                self.clear_start()
+            else: # Invalid action reward.
                 self.reward = penalty
+            # end if
+        # end if
+
         self.tmp_reward += (self.reward - 100)
-#        print("PASSING")
         return (self.observation, self.reward)
     # end def
+
     def clear_start(self):
-#        print("Game Over! Starting New Game...")
+        """ Restarts the game.
+        """
+
         self.tmp_reward = 0
         self.state = sitting
         self.tiger = left if random.randint(0,1) == 1 else right
-#        print(f"Tiger be set at the :{self.tiger}")
-        
+        print(f"Tiger is beind the :{self.tiger} door.")
+
     def print(self):
+        """ Returns a string indicating the status of the environment.
+        """
+
         print("==" * 20)
-        print(f"Total Reward:{self.tmp_reward+self.reward-100}")# Not able to show the last reward
-        
+        print(f"Total Reward:{self.tmp_reward + self.reward-100}")
+        # Not able to show the last reward
+
         print(f"Action Reward :{self.reward-100}")
         print(f"State :{tiger_state_enum[self.state]}")
         print(f"Actions :{tiger_action_enum[self.action]}")
