b606e1c72fe3806fd40ad5270615d1065810c948
Jiayan Liu <u6107041@anu.edu.au>
2019-10-08 06:13:33 +1100

    rewrite cheese_maze

--- pyaixi/environments/cheese_maze.py
+++ pyaixi/environments/cheese_maze.py
@@ -4,8 +4,6 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
-import inspect
-from pyaixi import environment, util
 import os
 import random
 import sys
@@ -15,26 +13,23 @@ import sys
 PROJECT_ROOT = os.path.realpath(os.path.join(os.pardir, os.pardir))
 sys.path.insert(0, PROJECT_ROOT)
 
+from pyaixi import environment, util
 
-# Defines an enumeration to represent agent action: moving in one of 4 possible directions.
+# Defines an enumeration to represent agent action: move up, down, left, right in the maze
 cheese_maze_action_enum = util.enum('up', 'down', 'left', 'right')
 
-# Defines an enumeration to represent agent observation:
-# There are six possible observations, numbered in a rather haphazard way.
-cheese_maze_observation_enum = util.enum(
-    five=5, seven=7, eight=8, nine=9, ten=10, twelve=12)
+# Defines an enumeration to represent agent observation: which aliased location the mouse is at
+cheese_maze_observation_enum = util.enum(five = 5, seven = 7, eight = 8, nine = 9, ten = 10, twelve = 12)
 
-# Defines an enumeration to represent agent reward:
-# bumping into wall: 0
-# moving into an empty cell: 9
-# getting the cheese: 20
-cheese_maze_reward_enum = util.enum(wall=0, move=9, cheese=20)
+# Defines an enumeration to represent agent reward: the agent took an invalid step or valid or highest rewarded step
+cheese_maze_reward_enum = util.enum(wall = 0, move = 9, cheese = 20)
 
 # Defines some shorthand notation for ease of reference.
 up = cheese_maze_action_enum.up
 down = cheese_maze_action_enum.down
 left = cheese_maze_action_enum.left
 right = cheese_maze_action_enum.right
+
 
 wall = cheese_maze_reward_enum.wall
 move = cheese_maze_reward_enum.move
@@ -47,126 +42,121 @@ nine = cheese_maze_observation_enum.nine
 ten = cheese_maze_observation_enum.ten
 twelve = cheese_maze_observation_enum.twelve
 
-# The actual maze has the shape:
-# 1,1,1,1,1,1,1
-# 1,9,10,8,10,12,1
-# 1,5,1,5,1,5,1
-# 1,7,1,7,1,7,1
-# 1,1,1,1,1,1,1
-# where 1 stands for walls.
-
-# The observed name of each cell in the coordinate system.
-location_alias = {(1, 1): nine, (1, 2): ten, (1, 3): eight, (1, 4): ten, (1, 5): twelve,
-                 (2, 1): five,              (2, 3): five,               (2, 5): five,
-                 (3, 1): seven,             (3, 3): seven,              (3, 5): seven}
-direction = {
-    '0': [1, 0],  # up
-    '1': [-1, 0],  # down
-    '2': [0, -1],  # left
-    '3': [0, 1]  # right
-}
-
-layout_filename = "cheese_maze.txt"
-sep = '/' if '/' in PROJECT_ROOT else '\\'
-path = os.path.dirname(
-        os.path.abspath(
-            inspect.getfile(
-                inspect.currentframe()))) + sep + layout_filename
-
-"""
-%%%%%%%
-%     %
-% % % %
-% % % %
-%%%%%%%
-"""
-
-# Construct the map.
-map = {}
-with open(path) as f:
-    row = 0
-    col = 0
-
-    for line in f:
-        for s in list(line.rstrip()):
-            if s == "%":
-                map[(row, col)] = s
-            else:
-                map[(row, col)] = location_alias[(row, col)]
-            col = ((col + 1) % 7)
-        row += 1
-print(map)
-
-
+alias_setting = {(1,1):nine, (1,2):ten, (1,3):eight, (1,4):ten, (1,5):twelve
+                 ,(2,1):five, (2,3):five, (2,5):five, (3,1):seven, (3,3):seven, (3,5):seven}
+# read maze file
+def readMazeFile():
+    layout = "cheese_maze.txt"
+    sep = '/' if '/' in PROJECT_ROOT else ''''\''''
+    import inspect
+    path = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe()))) + sep + layout
+    
+    # Setting up coordinates for maze
+    coordinator = {}
+    
+    with open(path) as f:
+        row = 0
+        col = 0
+    
+        for line in f:
+            for s in list(line.rstrip()):
+                if s == "%":
+                    coordinator[(row,col)] = s
+                else:
+                    coordinator[(row,col)] = alias_setting[(row,col)]
+                col=((col+1)%7)
+            row+=1
+        
 class CheeseMaze(environment.Environment):
-
-    def __init__(self, options={}):
-        environment.Environment.__init__(self, options=options)
-        self.maze = map
-        self.mouse = util.choice(
-            [x for x in map.keys() if map[x] != '%'])
-        self.cheese = util.choice(
-            [x for x in map.keys() if map[x] != '%'] and x != self.mouse)
-
+    """ A mouse and a cheese are in the same two-dimensional 7x5 maze. The task
+        is to get the cheese by taking least actions. 
+        
+        Action: Agent has only 4 choice of actions: moving up, down, left, right
+        in maze.
+        
+        Observations: each free cell has a location observation with decimal 
+        number([5,7,8,9,10,12]), and multiple locations may share one observa-
+        tion. The problem exhibits perceptual aliasing in that a single observ-
+        ation is potentially ambiguous.
+        
+        
+        Reward: Agent receive a penalty reward of 0 if it move towards a cell
+        occupied by wall(%), it receive a moving(9) reward by moving into
+        a free cell, it get the highest acceptable reward(30) if it moves into a 
+        location contains cheese.
+    """
+    
+    def __init__(self, options = {}):
+        # Set up the base environment.
+        environment.Environment.__init__(self, options = options)
+        self.maze = readMazeFile() # maze contains dcitionary of the environment
+        
+        self.cheese = (3,3) # permanent address of cheese
+        
         # Defines the acceptable action values.
         self.valid_actions = list(cheese_maze_action_enum.keys())
+
         # Defines the acceptable observation values.
         self.valid_observations = list(cheese_maze_observation_enum.keys())
+
         # Defines the acceptable reward values.
         self.valid_rewards = list(cheese_maze_observation_enum.keys())
 
-        self.observation = map[self.mouse]
+        # Initiate the game
+        self.clear_restart()
         self.reward = 0
 
     def perform_action(self, action):
         """ Receives the agent's action and calculates the new environment percept.
-
-        - `action`: action of the agent
         """
+        assert self.is_valid_action(action)
+        
+        # save the action
+        self.action = action
+        
+        # read coordinates of mouse
+        y,x = self.mouse
+        
+        # set current observation
+        observation = self.observation
+        
+        # movement effect of different direction
+        if action == up:
+            y -= 1
+        elif action == down:
+            y += 1
+        elif action == left:
+            x -= 1
+        else:
+            x += 1
+            
+        # agent bumps into a wall
+        if self.maze[y,x] == "%":
+#            print('hit wall')
+            self.reward = wall # penalty reward for bumping into wall
+        else:
+            self.mouse = (y,x) # update agent coordinate
+            observation = self.maze[self.mouse] # store the observation of current location
+            self.observation = observation 
+            self.reward = move # reward for a valid movement
+            if self.cheese == (y,x): # agent found the cheese
+#                print("YOU GOT CHEESE!!!!")
+                self.reward += cheese # adding cheese reward to self.reward result in max(reward) = 29 (5 bit)
+                self.clear_restart() # restart the game by resetting agent location and observation
+#                print(self.mouse)
+        return observation, self.reward
 
-    self.action = action
-    y, x = self.mouse
-    if action == up:
-        y -= 1
-    elif action == down:
-        y += 1
-    elif action == left:
-        x -= 1
-    else:
-        x += 1
-    # end if
+    def clear_restart(self):
+        self.mouse = (1,2) # initial position for agent
+        self.observation = self.maze[self.mouse] # initial observation (agent initial location)
+        
 
-    if map[y, x] == "%":
-        self.reward = wall
-    else:
-        self.reward = move
-        self.observation = map[y, x]
-        self.mouse = (y, x)
-        if self.cheese == (y, x):
-            self.reward += cheese
-    # end if
-
-    # return self.observation, self.action
-
-    # Store the observation and reward in the environment.
-    self.observation = observation
-    self.reward =
-
-    # return (observation, reward)
-
-#    def clear
-
-    def print(self):
-        """ Returns a string indicating the status of the environment.
-        """
-
-        message = "Action: " + \
-            (self.action) + \
-            ", observation: " + \
-            self.observation + \
-            ", reward: %d" % self.reward
-
-        return message
+#    def print(self):
+#        message = "Action: " + str(self.action) + ", observation" + str(self.observation) + ", reward: %d" %self.reward
+#        return message
 
 
-readMazeFile()
+
+
+
+
